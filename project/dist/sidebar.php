ba04fa8d3b04bc0317c02071ac969e0e4d7eaa8c
pyn18 <put.tho.1980@gmail.com>
2025-07-29 11:03:38 +0700

    first commit

---
+++ project/dist/sidebar.php
@@ -0,0 +1,33 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+  <!--begin::Sidebar Brand-->
+  <div class="sidebar-brand">
+    <!--begin::Brand Link-->
+    <a href="./index.html" class="brand-link">
+      <!--begin::Brand Image-->
+      <img src="./assets/img/logo.gif" alt="AdminLTE Logo" class="brand-image opacity-75 shadow" />
+      <!--end::Brand Image-->
+      <!--begin::Brand Text-->
+      <span class="brand-text fw-light">TATC-SHOP</span>
+      <!--end::Brand Text-->
+    </a>
+    <!--end::Brand Link-->
+  </div>
+  <!--end::Sidebar Brand-->
+  <!--begin::Sidebar Wrapper-->
+  <div class="sidebar-wrapper">
+    <nav class="mt-2">
+      <!--begin::Sidebar Menu-->
+      <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="navigation" aria-label="Main navigation"
+        data-accordion="false" id="navigation">
+        <li class="nav-item">
+          <a href="dashboard.php?page=dasboard" class="nav-link active">
+            <i class="nav-icon bi bi-palette"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+      </ul>
+      <!--end::Sidebar Menu-->
+    </nav>
+  </div>
+  <!--end::Sidebar Wrapper-->
+</aside>
